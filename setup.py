ae6f80ca51acad242cc5375a1447d7117607746b
Hanzhou Shi <hanzhou87@gmail.com>
2019-05-05 20:08:28 -0700

    Including filter module to the distribution.

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="ipng",
-    version="0.0.2",
+    version="0.0.3",
     author="hanjoes",
     author_email="hanzhou87@gmail.com",
     description="Intuitive PNG library.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/hanjoes/ipng",
-    py_modules=['ipng'],
+    py_modules=['ipng', 'filter'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
